9e21ba8bb27168278ee75935b732f200c6c8e455
kamurshes <kamurshes@menz-do.club>
2019-10-27 22:50:45 +0900

    初期化

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+$chatToken = getenv('chatToken');
+$chatGroupId = getenv('chatGroupId');
+$message = "This is test.\nMy name is test.";
+
+$headers = [
+    'X-ChatWorkToken: '.$chatToken
+];
+
+$option = [
+    'body' => $message
+];
+
+$ch = curl_init( 'https://api.chatwork.com/v2/rooms/'.$chatGroupId.'/messages' );
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($option));
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+
+?>
