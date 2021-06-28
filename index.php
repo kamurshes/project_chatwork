2835cfb6f6ed7309257415aef7eca72a96c8c7e1
Shunsuke Kawamura <kawamura@piccolospa.org>
2021-06-28 14:44:31 +0900

    Update index.php

--- index.php
+++ index.php
@@ -40,12 +40,13 @@ $tel=$_POST['data']['User']['tel'];
 // シナリオ
 $scenario=$_POST['data']['User']['scenario'];
 // セミナー申し込み日程
-$free4=$_POST['data']['User']['free4'];
+$free1=$_POST['data']['User']['free1'];
 
 $message = "シナリオ：". $_POST['data']['User']['scenario']."\n".
+	"オンライン説明会への参加日：".$free1."\n".
 	"氏名：".$name."\n".
 	"メールアドレス：".$email."\n".
-	"電話番号：".$tel."\n";
+	"電話番号：".$tel;
 
 $headers = [
     'X-ChatWorkToken: '.$chatToken
